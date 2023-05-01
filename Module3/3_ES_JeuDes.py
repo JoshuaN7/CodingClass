be108626973818e430cd63ba2512dc82da07173c
Joshua Nagy <josh8nagy@gmail.com>
2023-05-01 11:54:21 -0400

    pari

--- Module3/3_ES_JeuDes.py
+++ Module3/3_ES_JeuDes.py
@@ -9,12 +9,20 @@ import tkinter as tk
 
 # - Programme principal ------------------------------------------------------------------------------------------
 
+# - Variables globales
+argent = 100
+rondes = 0
+prediction = 0
+
 # - Déclaration des fonctions
+def lancer():
+    print("Lancer")
+    print(pari.get())
 
 # - Fenetre
 fenetre = tk.Tk()
 fenetre.title("Jeu de dés")
-fenetre.geometry("1000x500")
+fenetre.geometry("1200x500")
 fenetre['bg'] = "#ffffff"
 
 # - Création des cadres
@@ -23,16 +31,22 @@ cadreResultat = tk.Frame(fenetre)
 cadreArgent = tk.Frame(fenetre)
 
 # - Options cadreChoix
-cadreChoix['background'] = "#c900cc"
-cadreChoix.grid(row=0, column=0,columnspan=5, sticky="nsew")
+cadreChoix['background'] = "#ffffff"
+cadreChoix['relief'] = "ridge"
+cadreChoix['borderwidth'] = 3
+cadreChoix.grid(row=0, column=0,columnspan=5, rowspan=2, sticky="nsew")
 
 # - Options cadreResultat
-cadreResultat['background'] = "#ff276c"
-cadreResultat.grid(row=1, column=0, rowspan=3, columnspan=5, sticky="nsew")
+cadreResultat['background'] = "#ffffff"
+cadreResultat['relief'] = "ridge"
+cadreResultat['borderwidth'] = 3
+cadreResultat.grid(row=1, column=0, rowspan=4, columnspan=5, sticky="nsew")
 
 # - Options cadreArgent
-cadreArgent['background'] = "#0000FF"
-cadreArgent.grid(row=0, column=5, rowspan=4, sticky="nsew")
+cadreArgent['background'] = "#ffffff"
+cadreArgent['relief'] = "ridge"
+cadreArgent['borderwidth'] = 3
+cadreArgent.grid(row=0, column=4,columnspan=2, rowspan=5, sticky="nsew")
 
 # - Configuration des lignes et colonnes
 fenetre.columnconfigure(0, weight=1)
@@ -49,24 +63,84 @@ fenetre.rowconfigure(4, weight=1)
 
 # - Titre pour cadreChoix
 lblChoix = tk.Label(cadreChoix)
-lblChoix['text'] = "Choix"
-lblChoix['font'] = ["Arial", 30, "bold"]
-lblChoix['bg'] = "#c900cc"
-lblChoix.grid(row=0,column=0,columnspan=2, padx=100)
+lblChoix['text'] = "Prédisiez le gagnant du jeu"
+lblChoix['font'] = ["Calibri", 20, "bold"]
+lblChoix['bg'] = "#ffffff"
+lblChoix.grid(row=0,column=0,columnspan=3, padx=250)
+
+# - Option Alex
+radAlex = tk.Radiobutton(cadreChoix)
+radAlex['text'] = "Alex"
+radAlex['font'] = ["Calibri", 15, "bold"]
+radAlex['bg'] = "#ffffff"
+radAlex['value'] = 1
+radAlex['variable'] = prediction
+radAlex.select()
+radAlex.grid(row=1,column=0)
+
+# - Option Charles
+radCharles = tk.Radiobutton(cadreChoix)
+radCharles['text'] = "Charles"
+radCharles['font'] = ["Calibri", 15, "bold"]
+radCharles['bg'] = "#ffffff"
+radCharles['value'] = 2
+radCharles['variable'] = prediction
+radCharles.grid(row=1,column=1)
+
+# - Option Égalité
+radEgalite = tk.Radiobutton(cadreChoix)
+radEgalite['text'] = "Égalité"
+radEgalite['font'] = ["Calibri", 15, "bold"]
+radEgalite['bg'] = "#ffffff"
+radEgalite['value'] = 3
+radEgalite['variable'] = prediction
+radEgalite.grid(row=1,column=2)
+
+# - Bouton pour lancer les dés
+btnLancer = tk.Button(cadreChoix)
+btnLancer['text'] = "Lancer"
+btnLancer['font'] = ["Calibri", 15, "bold"]
+btnLancer['bg'] = "#ffffff"
+btnLancer['command'] = lancer
+btnLancer.grid(row=2,column=0,columnspan=3, padx=20)
 
 # - Titre pour cadreResultat
 lblResultat = tk.Label(cadreResultat)
 lblResultat['text'] = "Résultat"
-lblResultat['font'] = ["Arial", 30, "bold"]
+lblResultat['font'] = ["Calibri", 30, "bold"]
 lblResultat['bg'] = "#ffffff"
 lblResultat.grid(row=0,column=0,columnspan=2, padx=100)
 
 # - Titre pour cadreArgent
 lblArgent = tk.Label(cadreArgent)
 lblArgent['text'] = "Argent"
-lblArgent['font'] = ["Arial", 30, "bold"]
-lblArgent['bg'] = "#0000FF"
-lblArgent.grid(row=0,column=0,columnspan=2, padx=100)
+lblArgent['font'] = ["Calibri", 15, "bold"]
+lblArgent['bg'] = "#ffffff"
+lblArgent.grid(row=0,column=0,columnspan=2)
+
+# - Argent
+lblArgent = tk.Label(cadreArgent)
+lblArgent['text'] = str(argent) + "$"
+lblArgent['font'] = ["Calibri", 15, "bold"]
+lblArgent['bg'] = "#ffffff"
+lblArgent.grid(row=1,column=0,columnspan=2, padx=20)
+
+# - Argent parié
+lblArgentParie = tk.Label(cadreArgent)
+lblArgentParie['text'] = "Argent parié"
+lblArgentParie['font'] = ["Calibri", 15, "bold"]
+lblArgentParie['bg'] = "#ffffff"
+lblArgentParie.grid(row=2,column=0,columnspan=2)
+
+# - Argent input
+pari = tk.StringVar()
+entArgent = tk.Entry(cadreArgent)
+entArgent['font'] = ["Calibri", 15, "bold"]
+entArgent['bg'] = "#ffffff"
+entArgent['textvariable'] = pari
+entArgent.grid(row=3,column=0,columnspan=2, padx=20)
+
+
 
 
 
