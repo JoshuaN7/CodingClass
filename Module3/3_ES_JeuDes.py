a8f3e0ab0fef6d2118d0720a895c189d202dcf73
Joshua Nagy <josh8nagy@gmail.com>
2023-05-02 17:14:15 -0400

    rizz

--- Module3/3_ES_JeuDes.py
+++ Module3/3_ES_JeuDes.py
@@ -6,43 +6,160 @@
 
 # - Importaion des modules ------------------------------------------------------------------------------------------
 import tkinter as tk
+import random
 
 # - Programme principal ------------------------------------------------------------------------------------------
 
 # - Variables globales
-argent = 100
+global argent
+argent_defaut = 100
+argent = None
 rondes = 0
+personne = []
+
+
+# - Argent de départ
+# - Si le joueur a gagné ou perdu l'argent prendre la valeur de l'argent restant
+if argent == None:
+    argent = argent_defaut
 
 
 # - Déclaration des fonctions
-def lancer():
+
+# - Jouer
+
+def jouer():
+    global argent, personne, mpari
     # - Verifier si l'argent parié est un nombre
-    global prediction
+
     if pari.get().isdigit():
         print("Nombre")
         lblArgentError['text'] = ""
         if int(pari.get()) > argent:
             print("Pas assez d'argent")
-            lblArgentError['text'] = "Vous n'avez pas assez d'argent"
+            # - MAYBE ADD A THING TO CHECK IF THE NUMBER IS NEGATIVE
+            lblArgentError['text'] = "Vous n'avez pas assez d'argent. \n Vous avez {}$" .format(argent)
         else:
-            print("Assez d'argent")
+            # - Assigner la valeur du pari à la variable mpari
+            mpari = int(pari.get())
+            # - Enlever le texte de lblArgentError
             lblArgentError['text'] = ""
-            # - Lancer les dés
             # - Si Alex est prédit
-            if prediction == "1":
-                print("Prédiction Alex")
+            if prediction.get() == "1":
+                personne.append(1)
             # - Si Charles est prédit
-            elif prediction == "2":
-                print("Prédiction Charles")
+            elif prediction.get() == "2":
+                personne.append(2)
             # - Si Nul est prédit
-            elif prediction == "3":
-                print("Prédiction Nul")
+            elif prediction.get() == "3":
+                personne.append(3)
+            else :
+                lblArgentError['text'] = "Veuillez choisir une prédiction"
+            # - Lancer les dés
+            lancer()
     else:
         print("Pas un nombre")
         lblArgentError['text'] = "Veuillez entrer un nombre"
 
     print("Lancer")
     print(pari.get())
+
+# - Lancer les dés
+def lancer():
+    # - Vairables globales
+    global argent, personne, rf, mpari
+    # - Lancer les dés de Alex
+    d_o_1 = random.randint(1, 6)
+    d_o_2 = random.randint(1, 6)
+    d_o_3 = random.randint(1, 6)
+    lblAlex1['text'] = d_o_1
+    lblAlex2['text'] = d_o_2
+    lblAlex3['text'] = d_o_3
+
+    # - Lancer les dés de Charles
+    d_j_1 = random.randint(1, 6)
+    d_j_2 = random.randint(1, 6)
+    d_j_3 = random.randint(1, 6)
+    lblCharles1['text'] = d_j_1
+    lblCharles2['text'] = d_j_2
+    lblCharles3['text'] = d_j_3
+
+    # - Calculer la somme des dés d'Alex et de Charles
+    somme_o = d_o_1 + d_o_2 + d_o_3
+    somme_j = d_j_1 + d_j_2 + d_j_3
+
+    # - Prendre en compte les exceptions pour Alex
+    if d_o_1 == d_o_2 == d_o_3:
+        r_o = 8 + somme_o
+    elif d_o_1 != d_o_2 != d_o_3:
+        r_o = 5 + somme_o
+    else:
+        r_o = somme_o
+
+    # - Prendre en compte les exceptions pour Charles
+    if d_j_1 == d_j_2 == d_j_3:
+        r_j = 8 + somme_j
+    elif d_j_1 != d_j_2 != d_j_3:
+        r_j = 5 + somme_j
+    else:
+        r_j = somme_j
+
+    lblAlex['text'] = "Dés d'Alex: {}".format(r_o)
+    lblCharles['text'] = "Dés de Charles: {}".format(r_j)
+
+    # - Déterminer qui a gagné
+    if r_o > r_j:
+        r = "Alex a gagné!"
+        lblGagnant['text'] = "Alex a gagné!"
+    elif r_j > r_o:
+        r = "Charles a gagné!"
+        lblGagnant['text'] = "Charles a gagné!"
+    else:
+        r = "Match nul!"
+        lblGagnant['text'] = "Match nul!"
+
+   # - Si Alex a gagné
+    if r == "Alex a gagné!":
+        if personne == 1:
+            argent += mpari
+            lblArgentRestant['text'] = ("\nVous avez gagné {}$!".format(mpari))
+        elif personne == 2 or personne == 3:
+            argent -= mpari
+            lblArgentRestant['text'] = ("\nVous avez perdu {}$!".format(mpari))
+
+    # - Si Charles a gagné
+    elif r == "Charles a gagné!":
+        if personne == 2:
+            argent += mpari
+            lblArgentRestant['text'] = ("\nVous avez gagné {}$!".format(mpari))
+        elif personne == 1 or personne == 3:
+            argent -= mpari
+            lblArgentRestant['text'] = ("\nVous avez perdu {}$!".format(mpari))
+
+    # - Si le match est nul
+    elif r == "Match nul!":
+        if personne == 3:
+            argent += mpari
+            lblArgentRestant['text'] = ("\nVous avez gagné {}$!".format(mpari))
+        elif personne == 1 or personne == 2:
+            argent -= mpari
+            lblArgentRestant['text'] = ("\nVous avez perdu {}$!".format(mpari))
+
+    # - Afficher Erreur
+    else:
+        print("Erreur")
+
+    # # - Si le joueur a perdu tout son argent
+    # if argent <= 0:
+    #     print("\n{0:-^50}\n".format(" Résultats "))
+    #     print("Dés d'Alex: {} - {} - {}\nScore final d'Alex: {}".format(d_o_1, d_o_2, d_o_3, r_o))
+    #     print("\nDés de Charles: {} - {} - {}\nScore final de Charles: {}".format(d_j_1, d_j_2, d_j_3, r_j))
+    #     print("\n" + r)
+    #     print("{0:-^50}".format(" Argent "))
+    #     print(rf)
+    #     print("\nVous avez maintenant {}$.".format(argent))
+    #     print("\nVous avez perdu tout votre argent!")
+    #     print("\n{0:-^50}".format(" Fin du jeu "))
 
 # - Fenetre
 fenetre = tk.Tk()
@@ -93,9 +210,6 @@ lblChoix['font'] = ["Calibri", 20, "bold"]
 lblChoix['bg'] = "#ffffff"
 lblChoix.grid(row=0,column=0,columnspan=3, padx=250)
 
-# - Variable pour prediction
-global prediction
-
 prediction = tk.StringVar()
 
 # - Option Alex
@@ -131,7 +245,7 @@ btnLancer = tk.Button(cadreChoix)
 btnLancer['text'] = "Lancer"
 btnLancer['font'] = ["Calibri", 15, "bold"]
 btnLancer['bg'] = "#ffffff"
-btnLancer['command'] = lancer
+btnLancer['command'] = jouer
 btnLancer.grid(row=2,column=0,columnspan=3, padx=20)
 
 # - Titre pour cadreResultat
@@ -176,5 +290,72 @@ lblArgentError['text'] = ""
 lblArgentError['font'] = ["Calibri", 15, "bold"]
 lblArgentError['bg'] = "#ffffff"
 lblArgentError.grid(row=4,column=0,columnspan=2)
+
+# - Resultats Alex
+lblAlex1 = tk.Label(cadreResultat)
+lblAlex1['text'] = "0"
+lblAlex1['font'] = ["Calibri", 15, "bold"]
+lblAlex1['bg'] = "#ffffff"
+lblAlex1.grid(row=1,column=0)
+
+lblAlex2 = tk.Label(cadreResultat)
+lblAlex2['text'] = "0"
+lblAlex2['font'] = ["Calibri", 15, "bold"]
+lblAlex2['bg'] = "#ffffff"
+lblAlex2.grid(row=1,column=1)
+
+lblAlex3 = tk.Label(cadreResultat)
+lblAlex3['text'] = "0"
+lblAlex3['font'] = ["Calibri", 15, "bold"]
+lblAlex3['bg'] = "#ffffff"
+lblAlex3.grid(row=1,column=2)
+
+# - Label Alex
+lblAlex = tk.Label(cadreResultat)
+lblAlex['text'] = "Dés d'Alex"
+lblAlex['font'] = ["Calibri", 15, "bold"]
+lblAlex['bg'] = "#ffffff"
+lblAlex.grid(row=2,column=0, columnspan= 3)
+
+
+# - Resultats Charles
+lblCharles1 = tk.Label(cadreResultat)
+lblCharles1['text'] = "0"
+lblCharles1['font'] = ["Calibri", 15, "bold"]
+lblCharles1['bg'] = "#ffffff"
+lblCharles1.grid(row=1,column=3)
+
+lblCharles2 = tk.Label(cadreResultat)
+lblCharles2['text'] = "0"
+lblCharles2['font'] = ["Calibri", 15, "bold"]
+lblCharles2['bg'] = "#ffffff"
+lblCharles2.grid(row=1,column=4)
+
+lblCharles3 = tk.Label(cadreResultat)
+lblCharles3['text'] = "0"
+lblCharles3['font'] = ["Calibri", 15, "bold"]
+lblCharles3['bg'] = "#ffffff"
+lblCharles3.grid(row=1,column=5)
+
+# - Dés Charles
+lblCharles = tk.Label(cadreResultat)
+lblCharles['text'] = "Dés de Charles"
+lblCharles['font'] = ["Calibri", 15, "bold"]
+lblCharles['bg'] = "#ffffff"
+lblCharles.grid(row=2,column=3, columnspan= 3)
+
+# - Qui a gagné
+lblGagnant = tk.Label(cadreResultat)
+lblGagnant['text'] = ""
+lblGagnant['font'] = ["Calibri", 15, "bold"]
+lblGagnant['bg'] = "#ffffff"
+lblGagnant.grid(row=3,column=0, columnspan= 3)
+
+# - Argent restant
+lblArgentRestant = tk.Label(cadreResultat)
+lblArgentRestant['text'] = ""
+lblArgentRestant['font'] = ["Calibri", 15, "bold"]
+lblArgentRestant['bg'] = "#ffffff"
+lblArgentRestant.grid(row=4,column=0, columnspan= 3)
 
 fenetre.mainloop()
